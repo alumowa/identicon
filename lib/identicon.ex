82a1dd281db48e91fd1a5888fbd3805bccdbc224
Alvin Lumowa <alvin@venstar.com>
2017-09-26 00:50:18 -0700

    Add image generation & saving

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,13 +1,16 @@
 defmodule Identicon do
 
-  def generate(input) do
+  def generate(input, path) do
 
+    #Generation pipeline
     input
       |> hash_input
       |> pick_color
       |> build_grid
       |> filter_odd_squares
       |> build_pixel_map
+      |> draw_image
+      |> save_image("#{path}/#{input}.png")
   end
 
   defp hash_input(input) do
@@ -62,6 +65,24 @@ defmodule Identicon do
     end
 
     %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  defp draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  defp save_image(image, file) do
+
+    :egd.save(image, file)
   end
 
 end
